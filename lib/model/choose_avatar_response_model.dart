f29a225decb3c8ca565c98d264c6ed33ab883edc
ahithmohan <ahithmohan17@gmail.com>
2025-05-08 12:02:33 +0530

    add step in choose_avatar_response_model.dart model

--- lib/model/choose_avatar_response_model.dart
+++ lib/model/choose_avatar_response_model.dart
@@ -1,10 +1,27 @@
+// class ChooseAvatarResponseModel {
+//   final String status;
+//
+//   ChooseAvatarResponseModel({required this.status});
+//
+//   // Convert a JSON map into a ChooseAvatarResponseModel.
+//   factory ChooseAvatarResponseModel.fromJson(Map<String, dynamic> json) {
+//     return ChooseAvatarResponseModel(status: json['status']);
+//   }
+// }
 class ChooseAvatarResponseModel {
   final String status;
+  final int step;
 
-  ChooseAvatarResponseModel({required this.status});
+  ChooseAvatarResponseModel({
+    required this.status,
+    required this.step,
+  });
 
   // Convert a JSON map into a ChooseAvatarResponseModel.
   factory ChooseAvatarResponseModel.fromJson(Map<String, dynamic> json) {
-    return ChooseAvatarResponseModel(status: json['status']);
+    return ChooseAvatarResponseModel(
+      status: json['status'],
+      step: json['data']['step'],
+    );
   }
 }
