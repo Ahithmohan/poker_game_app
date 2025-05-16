607136eba6c55c3869b85a0ec336740b0df79d23
ahithmohan <ahithmohan17@gmail.com>
2025-05-16 11:22:19 +0530

    back button added

--- lib/view/qr_scan_page.dart
+++ lib/view/qr_scan_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
 
+import '../constants/screen_size.dart';
 import '../provider/qr_provider.dart';
 
 class QrScanPage extends StatefulWidget {
@@ -26,31 +27,53 @@ class _QrScanPageState extends State<QrScanPage> {
     final qrProvider = Provider.of<QrProvider>(context, listen: false);
 
     return Scaffold(
-      backgroundColor: Colors.black,
       body: Stack(
         children: [
-          Center(
-            child: SizedBox(
-              height: 350,
-              width: 350,
-              child: MobileScanner(
-                controller: controller,
-                onDetect: (capture) {
-                  if (!isScanned) {
-                    setState(() {
-                      isScanned = true;
-                    });
-                    final barcode = capture.barcodes.first;
-                    if (barcode.rawValue != null) {
-                      // Update the QR code value in the provider
-                      qrProvider.setScannedCode(barcode.rawValue!);
-                      // Navigate back after updating the provider
-                      Navigator.pop(context);
-                    }
-                  }
-                },
+          Container(
+            height: ScreenSize.screenHeight,
+            width: ScreenSize.screenWidth,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/lobby/lobby pattern.png"),
+                fit: BoxFit.cover,
               ),
             ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: SizedBox(
+                  height: 350,
+                  width: 350,
+                  child: MobileScanner(
+                    controller: controller,
+                    onDetect: (capture) {
+                      if (!isScanned) {
+                        setState(() {
+                          isScanned = true;
+                        });
+                        final barcode = capture.barcodes.first;
+                        if (barcode.rawValue != null) {
+                          // Update the QR code value in the provider
+                          qrProvider.setScannedCode(barcode.rawValue!);
+                          // Navigate back after updating the provider
+                          Navigator.pop(context);
+                        }
+                      }
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Image.asset("assets/genderpage/back_btn.png"))
+            ],
           ),
         ],
       ),
